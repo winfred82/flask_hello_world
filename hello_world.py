9c363d4cdebe079dce10a98d26ebd63bf068c4dc
Winfred Zhu <winfredzhu@outlook.com>
2015-12-14 09:20:00 +0000

    flask task

---
+++ hello_world.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from os import environ
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/hello")
+def say_hi():
+    return "Hello World!"
+
+@app.route("/hello/<name>")
+def hi_person(name):
+    html = """
+        <h1>
+            Hello {}!
+        </h1>
+        <p>
+            Here's a picture of a kitten.  Awww...
+        </p>
+        <img src="http://placekitten.com/g/200/300">
+    """
+    return html.format(name.title())
+
+if __name__ == "__main__":
+    app.run(host=environ['IP'],
+            port=int(environ['PORT']))
